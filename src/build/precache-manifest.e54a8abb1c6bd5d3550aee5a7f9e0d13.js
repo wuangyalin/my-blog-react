9facadbceb9cd8ac2d62d173d93ffe8d3d96e76f
Luke Gong <lukegong.it@gmail.com>
2020-05-28 21:45:34 +1000

    first commit

---
+++ src/build/precache-manifest.e54a8abb1c6bd5d3550aee5a7f9e0d13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d08858dca7fb31863d55063ec0d52887",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4f13620c3617b34b3a3",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "d83bfc710fc92b622efb",
+    "url": "/static/js/2.9a8e5e93.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9a8e5e93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4f13620c3617b34b3a3",
+    "url": "/static/js/main.6f140ad9.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
